8aa0e043c45574409023dd6a0b3f742e5cdfa59d
이진중 <ijinjung@ijinjungs-MacBook-Pro.local>
2022-09-08 17:55:45 +0900

    build함수 재정의

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatefulWidget{
   @override
   State<StatefulWidget> createState(){
+    print('createState');
     return _MyApp();
   }
 }
@@ -21,32 +22,9 @@ class _MyApp extends State<MyApp> {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.amber,
       ),
-      darkTheme: ThemeData.light(),
-      home: Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            child: Text('$test'),
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(_color)),
-            onPressed: (){
-              if (_color==Colors.blue){
-                setState(() {
-                  test='flutter';
-                  _color=Colors.amber;
-                });
-              } else{
-                setState(() {
-                  test='flutter';
-                  _color=Colors.blue;
-                });
-              }
-            },
-          ),
-        ),
-      )
+      home: MyHomePage(title: 'Flutter Demo Home Page',),
     );
 
   }
